c0bb7cc318d2089d1c6b2b338ffc09746e932a8a
Angela Wolff <warriorwomenblog@gmail.com>
2018-12-08 20:40:49 +0000

    Adding express server for CRA

---
+++ build/precache-manifest.f76a29ea16660b739234c5841a12ebe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6060ea0442304d7d8257",
+    "url": "/static/js/main.6060ea04.chunk.js"
+  },
+  {
+    "revision": "8adaa70410dab749d659",
+    "url": "/static/js/1.8adaa704.chunk.js"
+  },
+  {
+    "revision": "6060ea0442304d7d8257",
+    "url": "/static/css/main.71f12b61.chunk.css"
+  },
+  {
+    "revision": "3f53c2cfb8c46a97f493251ec6662c33",
+    "url": "/index.html"
+  }
+];
